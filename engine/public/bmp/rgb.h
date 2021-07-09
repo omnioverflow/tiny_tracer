9222308ceab36d1d41d814b50ba1f006984e678c
iirthw <iirthw@gmail.com>
2021-07-09 15:56:44 +0300

    [engine] add rgb struct stub

---
+++ engine/public/bmp/rgb.h
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace engine
+{
+  struct Rgba
+  {
+    unsigned char mReg;
+    unsigned char mGreen;
+    unsigned char mBlue;
+    unsigned char mAlpha;
+  }; // Rgba
+} // namespace engine
