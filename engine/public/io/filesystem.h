dc76ca2480cbb8158eb3d862ad170a0e98e0ccfd
iirthw <iirthw@gmail.com>
2021-07-09 15:57:05 +0300

    [engine/io] wip on filesystem header

---
+++ engine/public/io/filesystem.h
@@ -0,0 +1,10 @@
+// #ifndef INCLUDE_STD_FILESYSTEM
+//  #ifdef __cpp_lib_filesystem
+//        #include <filesystem>
+//        // using std::filesystem;
+//  #elseif __cpp_lib_experimental_filesystem
+//        #include <experimental/filesystem>
+//        using fs = std::experimental::filesystem;
+////        #else
+////        #error "No std::filesystem support"
+//  #endif
