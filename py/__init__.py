dc190a38068f410fddda98ed8830f8e9c71b4dec
iirthw <iirthw@gmail.com>
2021-07-12 23:51:55 +0300

    [py] add dummy implementation for the __init__.py and path_tracer.py

---
+++ py/__init__.py
@@ -0,0 +1,1 @@
+# TODO: implement __init__.py
---
+++ py/path_tracer.py
@@ -0,0 +1,7 @@
+def main():
+    # FIXME: implement the main
+    str = "Olololo-trololo-trololo-trololo"
+    print(str)
+
+if __name__=="__main__":
+    main()
