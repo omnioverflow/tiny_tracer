200f25a1e24eb44da78ae910ff2282e7d2c5be83
iirthw <iirthw@gmail.com>
2021-07-16 12:21:33 +0300

    [py] Show tkinter window (test)

--- py/path_tracer.py
+++ py/path_tracer.py
@@ -1,7 +1,15 @@
+from tkinter import *
+
 def main():
     # FIXME: implement the main
     str = "Olololo-trololo-trololo-trololo"
     print(str)
+
+    window = Tk()
+    canvas = Canvas(window, width = 500, height = 500)
+    canvas.pack()
+
+    window.mainloop()
 
 if __name__=="__main__":
     main()
